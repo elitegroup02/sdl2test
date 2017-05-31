68973c5543a9f1bb343a9e8ffae16363ebeadc3c
Juan Pablo Rodriguez Acevedo <juanpablorodriguezacevedo@gmail.com>
2017-05-30 22:50:18 -0300

    Add project files.

---
+++ sdl2test/Source.cpp
@@ -0,0 +1,155 @@
+#include <string>
+#include <iostream>
+#include <SDL.h>
+#include "res_path.h"
+#include "cleanup.h"
+using namespace std;
+
+/*
+* Lesson 2: Don't Put Everything in Main
+*/
+//Screen attributes
+int SCREEN_WIDTH = 1280;
+int SCREEN_HEIGHT = 720;
+
+/*
+* Log an SDL error with some error message to the output stream of our choice
+* @param os The output stream to write the message too
+* @param msg The error message to write, format will be msg error: SDL_GetError()
+*/
+void logSDLError( ostream &os, const  string &msg) {
+	os << msg << " error: " << SDL_GetError() <<  endl;
+}
+/*
+* Loads a BMP image into a texture on the rendering device
+* @param file The BMP image file to load
+* @param ren The renderer to load the texture onto
+* @return the loaded texture, or nullptr if something went wrong.
+*/
+SDL_Texture* loadTexture(const  string &file, SDL_Renderer *ren) {
+	SDL_Texture *texture = nullptr;
+	//Load the image
+	SDL_Surface *loadedImage = SDL_LoadBMP(file.c_str());
+	//If the loading went ok, convert to texture and return the texture
+	if (loadedImage != nullptr) {
+		texture = SDL_CreateTextureFromSurface(ren, loadedImage);
+		SDL_FreeSurface(loadedImage);
+		//Make sure converting went ok too
+		if (texture == nullptr) {
+			logSDLError( cout, "CreateTextureFromSurface");
+		}
+	}
+	else {
+		logSDLError( cout, "LoadBMP");
+	}
+	return texture;
+}
+/*
+* Draw an SDL_Texture to an SDL_Renderer at position x, y, preserving
+* the texture's width and height
+* @param tex The source texture we want to draw
+* @param ren The renderer we want to draw too
+* @param x The x coordinate to draw too
+* @param y The y coordinate to draw too
+*/
+void renderTexture(SDL_Texture *tex, SDL_Renderer *ren, int x, int y) {
+	//Setup the destination rectangle to be at the position we want
+	SDL_Rect dst;
+	dst.x = x;
+	dst.y = y;
+	//Query the texture to get its width and height to use
+	SDL_QueryTexture(tex, NULL, NULL, &dst.w, &dst.h);
+	SDL_RenderCopy(ren, tex, NULL, &dst);
+}
+
+int ximage = 0;
+int yimage = 0;
+
+int main(int, char**) {
+	//Start up SDL and make sure it went ok
+	if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+		logSDLError( cout, "SDL_Init");
+		return 1;
+	}
+
+	//Setup our window and renderer
+	SDL_Window *window = SDL_CreateWindow("Lesson 2", 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+	if (window == nullptr) {
+		logSDLError( cout, "CreateWindow");
+		SDL_Quit();
+		return 1;
+	}
+	SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+	if (renderer == nullptr) {
+		logSDLError( cout, "CreateRenderer");
+		cleanup(window);
+		SDL_Quit();
+		return 1;
+	}
+
+	//The textures we'll be using
+	SDL_Texture *background = loadTexture("C:/Users/Juan Pablo/Documents/Visual Studio 2017/Projects/sdl2test/sdl2test/background.bmp", renderer);
+	SDL_Texture *image = loadTexture("C:/Users/Juan Pablo/Documents/Visual Studio 2017/Projects/sdl2test/sdl2test/image.bmp", renderer);
+	SDL_Texture *ju = loadTexture("C:/Users/Juan Pablo/Documents/Visual Studio 2017/Projects/sdl2test/sdl2test/ju.bmp", renderer);
+	SDL_Texture *an = loadTexture("C:/Users/Juan Pablo/Documents/Visual Studio 2017/Projects/sdl2test/sdl2test/an.bmp", renderer);
+	SDL_Texture *pi = loadTexture("C:/Users/Juan Pablo/Documents/Visual Studio 2017/Projects/sdl2test/sdl2test/pi.bmp", renderer);
+	
+	//Make sure they both loaded ok
+	if (background == nullptr || image == nullptr || ju == nullptr || an == nullptr || pi == nullptr) {
+		cleanup(background, image, renderer, window);
+		SDL_Quit();
+		return 1;
+	}
+
+	//A sleepy rendering loop, wait for 3 seconds and render and present the screen each time
+	for (int i = 0; i < 100000; ++i) {
+		//Clear the window
+		SDL_RenderClear(renderer);
+
+		//Get the width and height from the texture so we know how much to move x,y by
+		//to tile it correctly
+		int bW, bH;
+		SDL_QueryTexture(background, NULL, NULL, &bW, &bH);
+		//We want to tile our background so draw it 4 times
+		renderTexture(background, renderer, 0, 0);
+		renderTexture(background, renderer, bW, 0);
+		renderTexture(background, renderer, 2 * bW, 0);
+		renderTexture(background, renderer, 3 * bW, 0);
+		renderTexture(background, renderer, 0, bH);
+		renderTexture(background, renderer, bW, bH);
+		renderTexture(background, renderer, 2 * bW, bH);
+		renderTexture(background, renderer, 3 * bW, bH);
+		renderTexture(background, renderer, 0, 2* bH);
+		renderTexture(background, renderer, bW, 2* bH);
+		renderTexture(background, renderer, 2 * bW, 2* bH);
+		renderTexture(background, renderer, 3 * bW, 2* bH);
+
+		//Draw our image in the center of the window
+		//We need the foreground image's width to properly compute the position
+		//of it's top left corner so that the image will be centered
+		int iW, iH;
+		SDL_QueryTexture(image, NULL, NULL, &iW, &iH);
+
+		if (ximage >= 1080) {
+			ximage = 0;
+			yimage = yimage + 200;
+
+		}
+		else {
+			ximage = ximage + 4;
+		}
+		
+		renderTexture(image, renderer, ximage, yimage);
+
+
+		//Update the screen
+		SDL_RenderPresent(renderer);
+		//Take a quick break after all that hard work
+		SDL_Delay(10);
+	}
+
+	cleanup(background, image, renderer, window);
+	SDL_Quit();
+
+	return 0;
+}
